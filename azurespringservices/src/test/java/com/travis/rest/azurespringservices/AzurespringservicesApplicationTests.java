a965abd000ba0ca3f4d676cc745b137558f7fb1f
Travis Le <travisle247@hotmail.com>
2019-01-26 11:11:08 -0600

    Initial commit

---
+++ azurespringservices/src/test/java/com/travis/rest/azurespringservices/AzurespringservicesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.travis.rest.azurespringservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AzurespringservicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
---
+++ src/main/java/com/travis/rest/azurespringservices/AzurespringservicesApplication.java
@@ -0,0 +1,14 @@
+package com.travis.rest.azurespringservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurespringservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurespringservicesApplication.class, args);
+	}
+
+}
+
